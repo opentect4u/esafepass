1ccae1bcfa6692ece962b39fe6d81dc5175422fb
meettan <meettan@synergicsoftek.com>
2023-02-16 16:17:54 +0530

    02192023

--- app/application/config/database.php
+++ app/application/config/database.php
@@ -77,7 +77,7 @@ $db['default'] = array(
 	'dsn'	   => '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'eSafe#Pass@2023',
 	'database' => 'esafepas_application',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
@@ -94,3 +94,26 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+
+// $db['default'] = array(
+// 	'dsn'	   => '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'esafepas_application',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt'  => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
