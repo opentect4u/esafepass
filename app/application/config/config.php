1dd2493f18c8f016f8fd6ff8e1d33fea515a633c
meettan <meettan@synergicsoftek.com>
2023-02-16 16:18:41 +0530

    baseurl

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/esafepass/app/';
+// $config['base_url'] = 'http://localhost/esafepass/app/';
+$config['base_url'] = 'https://esafepass.com/';
 
 /*
 |--------------------------------------------------------------------------
